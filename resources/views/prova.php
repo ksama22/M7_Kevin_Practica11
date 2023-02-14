cc12bd301f762a33a71639d1b5c092b4b6cc659e
ksama22 <ksama22@jaumebalmes.net>
2023-02-14 20:41:25 +0100

    Primeres rutes

--- resources/views/prova.php
+++ resources/views/prova.php
@@ -8,6 +8,6 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     </head>
     <body>
-      Hello World
+      Hola gente
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/test', function () {
+    return view('prova');
+});
+
+Route::get('/testing', function () {
+    return "Retorna tal cual";
+});
+
