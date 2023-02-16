54a83fa56d492e6db6e84b306a192bf2ed840cbc
ksama22 <ksama22@jaumebalmes.net>
2023-02-16 22:32:11 +0100

    Path amb parametres

--- app/Http/Controllers/ControladorEjercicio1.php
+++ app/Http/Controllers/ControladorEjercicio1.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 class ControladorEjercicio1 extends Controller
 {
     public function ej1Parametro(){
-        return "Hola Roger :D, soy un 'ej1Parametro'";
+        return "Éste es el resultado del primer ejercicio de la 
+        práctica hecha por Kevin Sama";
+    }
+
+
+    public function ej1ParametroConVariable($texto=''){
+        $textoFinal = "Sabias que ";
+        $textoFinal .= ($texto == '')?'en terminos':$texto;
+        return view('PrimeraView')->with(['texto'=>$textoFinal]);
+    }
+
+    public function hola(){
+        return view('PrimeraView')-> with(['texto'=>'Vaporeon Blanco']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,15 @@ use App\Http\Controllers\ControladorEjercicio1;
 | Web Routes
 |--------------------------------------------------------------------------
 */
+
+//Ruta con path , llama la funcion del controlador
 Route::get('/ejercicio1', [ControladorEjercicio1 ::class,'ej1Parametro']);
 
+//Ruta con path/{variable} y parametros, llama la funcion del controlador
+Route::get('/hola/{texto?}', [ControladorEjercicio1 ::class,
+'ej1ParametroConVariable']);
 
+//Ruta por defecto
 Route::get('/', function () {
     return view('welcome');
 });
@@ -22,7 +28,7 @@ Route::get('/test', function () {
 Route::get('/testing', function () {
     return "Retorna tal cual";
 });
-//Llama el Controlador creador y la primera funcion
 
 
-
+//En la ruta 'ejercicio1' usa el controlador 'ControladorEjercicio1' i usa la funcion 'ej1Parametro'
+//Route::get('/ejercicio1', [ControladorEjercicio1 ::class,'ej1Parametro']);
