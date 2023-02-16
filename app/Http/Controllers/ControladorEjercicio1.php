37fcf471678af19da5560b97deb082e23a3c1792
ksama22 <ksama22@jaumebalmes.net>
2023-02-16 19:30:32 +0100

    Exercici 1

---
+++ app/Http/Controllers/ControladorEjercicio1.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ControladorEjercicio1 extends Controller
+{
+    public function ej1Parametro(){
+        return "Hola Roger :D, soy un 'ej1Parametro'";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 //Carta el controlador creador 
-use App\Http\Controllers\PrimerControlador;
+use App\Http\Controllers\ControladorEjercicio1;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
+Route::get('/ejercicio1', [ControladorEjercicio1 ::class,'ej1Parametro']);
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,8 +23,6 @@ Route::get('/testing', function () {
     return "Retorna tal cual";
 });
 //Llama el Controlador creador y la primera funcion
-Route::get('/pepe', [PrimerControlador::class,'arrel']);
 
 
-Route::get('/ejercicio1', [ControladorEjercicio1 ::class,'ej1Parametro']);
 
